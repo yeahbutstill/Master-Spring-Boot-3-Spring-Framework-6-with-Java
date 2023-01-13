b447c60ae35b5200a104bac285b8fdfe5d6c89ad
yeahbutstill <mayadani28@gmail.com>
2023-01-13 21:31:50 +0700

    implement mvc

---
+++ section7/src/main/java/com/yeahbutstill/Section7Application.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section7Application.class, args);
+	}
+
+}
---
+++ section7/src/main/java/com/yeahbutstill/controller/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.yeahbutstill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+}
