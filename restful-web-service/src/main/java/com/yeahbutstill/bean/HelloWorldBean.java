8b081c27a74465cb43b7cce948eba316d91ad436
Dani Setiawan <mayadani28@gmail.com>
2023-01-19 22:34:39 +0700

    well let's go back to basic

---
+++ restful-web-service/src/main/java/com/yeahbutstill/bean/HelloWorldBean.java
@@ -0,0 +1,40 @@
+package com.yeahbutstill.bean;
+
+import java.util.Objects;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HelloWorldBean that = (HelloWorldBean) o;
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ restful-web-service/src/main/java/com/yeahbutstill/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.yeahbutstill.controller;
+
+import com.yeahbutstill.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+}
