8e4aedc991205a5904afac9000346d455443e7c2
Dani Setiawan <mayadani28@gmail.com>
2023-02-07 13:44:11 +0700

    Implement OAuth Google Authentication

---
+++ outhwithspingsecurity/src/main/java/com/yeahbutstill/outhwithspingsecurity/OuthwithspingsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.outhwithspingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuthwithspingsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuthwithspingsecurityApplication.class, args);
+	}
+
+}
---
+++ outhwithspingsecurity/src/main/java/com/yeahbutstill/outhwithspingsecurity/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.yeahbutstill.outhwithspingsecurity.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/oauth")
+public class HelloWorldController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/")
+    public String helloWorld(Authentication authentication) {
+
+        logger.info("is authenticated {}", authentication.isAuthenticated());
+        logger.info("authorities {}", authentication.getAuthorities());
+        logger.info("principal {}", authentication.getPrincipal());
+        logger.info("credentials {}", authentication.getCredentials());
+        logger.info("details {}", authentication.getDetails());
+
+        return "Hello World";
+    }
+
+}
