c61be6e7ddbcfcdc77410e0063c4b4d206b14178
Dani Setiawan <mayadani28@gmail.com>
2023-02-14 20:56:12 +0700

    Done exercies

--- functional-programming/src/main/java/org/example/FP01Exercises.java
+++ functional-programming/src/main/java/org/example/FP01Exercises.java
@@ -5,7 +5,14 @@ import java.util.List;
 public class FP01Exercises {
 
     public static void main(String[] args) {
-        printOddNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printOddNumbersInListStructured(numbers);
+        System.out.println();
+
+        printSquaresOfEvenNumberInListFunctional(numbers);
+        System.out.println();
+
+        printCubesOfOddNumberInListFunctional(numbers);
         System.out.println();
 
         List<String> courses = List.of("Spring", "Spring boot", "API",  "Microservice",
@@ -19,12 +26,31 @@ public class FP01Exercises {
                 .filter(course -> course.length() >= 4)
                 .forEach(System.out::println);
 
+        System.out.println();
+
+        courses.stream()
+                .map(course -> course + " " + course.length())
+                .forEach(System.out::println);
     }
 
     private static void printOddNumbersInListStructured(List<Integer> numbers) {
         numbers.stream() // Convert to stream
                 .filter(number -> number % 2 != 0) // Lambda expression
                 .forEach(System.out::println); // Method reference
+    }
+
+    private static void printSquaresOfEvenNumberInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .map(number -> number * number)
+                .forEach(System.out::println);
+    }
+
+    private static void printCubesOfOddNumberInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .map(number -> number * number)
+                .forEach(System.out::println);
     }
 
 }
